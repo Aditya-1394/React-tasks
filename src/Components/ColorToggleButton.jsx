c226126ef7b2834ef7a9be9284eadb9fe5ab26cb
Aditya <a.aditya1394@gmail.com>
2025-08-28 08:42:05 +0530

    Update ColorToggleButton.jsx

--- src/Components/ColorToggleButton.jsx
+++ src/Components/ColorToggleButton.jsx
@@ -1,22 +1,38 @@
  import React, { useState } from "react";
 
- const ColorToggleButton = () => {
-  const [isBlue, setIsBlue] = useState(true);
-  
-  return (
-    <button
-      onClick={() => setIsBlue(!isBlue)}
-      style={{
-        backgroundColor: isBlue ? "blue" : "green",
-        color: "white",
-        padding: "10px 20px",
-        border: "none",
-        cursor: "pointer",
-        borderRadius: "5px"
-      }}
-    >
-      {isBlue ? "Blue Button" : "Green Button"}
-    </button>
-  );
- };
- export default ColorToggleButton;
+class ColorToggleButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBlue: true, 
+    };
+  }
+
+  toggleColor = () => {
+    this.setState((prevState) => ({
+      isBlue: !prevState.isBlue,
+    }));
+  };
+
+  render() {
+    const { isBlue } = this.state;
+
+    const buttonStyle = {
+      backgroundColor: isBlue ? "blue" : "green",
+      color: "white",
+      padding: "10px 20px",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      fontSize: "16px",
+    };
+
+    return (
+      <button style={buttonStyle} onClick={this.toggleColor}>
+        {isBlue ? "Blue Button" : "Green Button"}
+      </button>
+    );
+  }
+}
+
+export default ColorToggleButton;
